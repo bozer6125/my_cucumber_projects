019e2d80709ae25f600784e020b7330bc6afeae2
bozer6125 <ozrbeyza@gmail.com>
2020-12-26 23:51:05 -0700

    Vytrack project files added

--- Summer2020CucumberTestAutomationProject.iml/src/test/java/com/vytrack/runners/1.java
+++ Summer2020CucumberTestAutomationProject.iml/src/test/java/com/vytrack/runners/1.java
@@ -1,9 +1,7 @@
 package com.vytrack.runners;
 
-
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
